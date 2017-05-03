4149b0c566b624088f69fcd47c0d3e26ac1bcc67
szatkus <tomasz.szatkowski@baroo.ie>
2017-05-03 10:34:32 +0200

    Setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -f config.py ]; then
+  echo "config.py not found"
+  exit
+fi
+
+python3 -m venv venv
+venv/bin/pip install -r requirements.txt
+echo -e "from database import db\ndb.create_all()" | venv/bin/python
+
+echo
+echo "Run "venv/bin/python main.py" to start."
