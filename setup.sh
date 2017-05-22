c1d677e109c1593d6ed465a610e1d07d2960219a
urbanner <urbanner@rocketmail.com>
2017-05-22 18:51:32 +0200

    fix to setup.sh

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ if [ ! -f config.py ]; then
 fi
 
 python3 -m venv venv
-venv\Scripts\pip install -r requirements.txt
-echo -e "from database import db\ndb.create_all()\import testdata" | venv/bin/python
+venv/bin/pip install -r requirements.txt
+echo -e "from database import db\ndb.create_all()\nimport testdata" | venv/bin/python
 
 echo
 echo "Run "venv/bin/python main.py" to start."
